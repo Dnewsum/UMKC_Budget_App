adf736d87c1b495815f030756a48d64cdfee4726
Daniel Newsum <drn7p3@umsystem.edu>
2025-04-19 18:46:42 -0500

    Local backup: fix imports and carousel images~

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,14 @@
 // src/main.tsx
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import Home from "./Home.tsx";
+import Home from "./pages/Home.tsx";
 import "./index.css";
 
 import { NavbarHome } from "./components/NavbarHome.tsx";
 
-
-
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <NavbarHome />
-      <Home />
-  </StrictMode>
+    <Home />
+  </StrictMode>,
 );
